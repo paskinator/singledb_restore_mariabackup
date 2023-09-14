5bd5b60348624ae6aa5b1ed3e01b9699868cf00a
Harry Pask <harry.pask118@gmail.com>
2023-09-14 13:22:31 +0100

    Update singledb.bash

--- singledb.bash
+++ singledb.bash
@@ -12,7 +12,7 @@ dropkeys='/tmp/drop.sql'
 addkeys='/tmp/add.sql'
 
 declare -a mariadboptions=(
-		"-u user"
+		"-ubackup"
 		"-ppassword"
 		)
 
@@ -29,18 +29,19 @@ echo -e "SET @databasename = '$dbname';\n$(cat grabresults.sql)" > grabresults2.
 
 #Apply data structure for $dbname
 #mariadb $dbname < /home/harrypask/scripts/testing/nodata.sql
-mariadb "${mariadboptions[@]}" $dbname < $nodatafile
+#"${mariadboptions[@]}"
+mariadb $dbname < $nodatafile
 
 #Pipe generated file into mariadb to export four .sql files for process
-mariadb "${mariadboptions[@]}" < grabresults2.sql
+mariadb < grabresults2.sql
 
 #drop keys
 echo "$(date +'%Y-%m-%d %H:%M:%S') Dropping keys for database $dbname"
-mariadb "${mariadboptions[@]}" $dbname < $dropkeys
+mariadb $dbname < $dropkeys
 
 #discard tablespace
 echo "$(date +'%Y-%m-%d %H:%M:%S') Discarding tablespaces for database $dbname"
-mariadb "${mariadboptions[@]}" $dbname < $discardtablespace
+mariadb $dbname < $discardtablespace
 
 #copy files
 cp $backup_dir/$dbname/*.cfg /var/lib/mysql/$dbname
